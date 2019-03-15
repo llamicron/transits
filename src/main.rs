fc4867171e02fde36502e08c9b5ca2cddab309df
llamicron <llamicron@gmail.com>
2019-03-15 13:07:37 -0500

    Moves static files out of src/

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,6 @@ use vartools::Vartools;
 use std::path::PathBuf;
 use std::fs;
 
-use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::json::{Json, JsonValue};
 use serde_derive::{Deserialize, Serialize};
 use data_formatter::DataFormatter;
@@ -33,7 +32,6 @@ struct VartoolsPayload {
     cmd: String,
     infile: String
 }
-
 
 
 #[post("/vartools", format = "application/json", data = "<payload>")]
@@ -86,7 +84,6 @@ fn not_found() -> JsonValue {
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .mount("/api", routes![handle_json])
-        .mount("/", StaticFiles::from("./src/static"))
         .register(catchers![not_found])
 }
 

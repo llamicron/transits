b8d67a58625535996a04fae1e94e128fc23ca17d
llamicron <llamicron@gmail.com>
2019-03-20 13:41:37 -0500

    Normalizes vartools arguments

--- interface/js/vartools.js
+++ interface/js/vartools.js
@@ -53,6 +53,14 @@
 //   index: 0
 // }
 
+function boolToInt(bool) {
+  if (bool) {
+    return "1";
+  } else {
+    return "0";
+  }
+}
+
 function uuid() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
@@ -77,6 +85,8 @@ vartools = {
       arguments: [
         {
           name: "r",
+          description: "Minimum and maximum stellar radius r to consider (in solar radii)",
+          type: 'text',
           id: uuid(),
           value: function () {
             if (this.rawValue.length > 0) {
@@ -87,12 +97,12 @@ vartools = {
           },
           rawValue: "",
           placeholder: "rmin rmax",
-          type: 'text',
-          description: "Minimum and maximum stellar radius r to consider (in solar radii)",
-          index: 0
+          index: 0,
         },
         {
           name: "q",
+          description: "Fixed minimum and maximum q (fraction of orbit in transit) for the search",
+          type: 'text',
           id: uuid(),
           value: function () {
             if (this.rawValue.length > 0) {
@@ -103,14 +113,14 @@ vartools = {
           },
           rawValue: "",
           placeholder: "qmin qmax",
-          type: 'text',
-          description: "Fixed minimum and maximum q (fraction of orbit in transit) for the search",
-          index: 0
+          index: 0,
         },
         {
           name: "density",
+          description: "stellar density (in grams per cubic centimeter) and a minimum and maximum fraction of the expected transit duration (assuming a circular orbit)",
+          type: 'text',
           id: uuid(),
-          value: function() {
+          value: function () {
             if (this.rawValue.length > 0) {
               return this.name + " " + this.rawValue;
             } else {
@@ -119,199 +129,195 @@ vartools = {
           },
           rawValue: "",
           placeholder: "rho minexpdurfrac maxexpdurfrac",
-          type: 'text',
-          description: "stellar density (in grams per cubic centimeter) and a minimum and maximum fraction of the expected transit duration (assuming a circular orbit)",
-          index: 0
+          index: 0,
         },
         {
           name: "minper",
+          description: "Minimum periods to search in days",
+          type: 'text',
           id: uuid(),
           value: function () {
             return this.rawValue;
           },
           rawValue: "",
-          placeholder: "",
-          type: 'text',
-          description: "Minimum periods to search in days",
-          index: 1
+          placeholder: "minper",
+          index: 1,
         },
         {
           name: "maxper",
+          description: "Maximum periods to search in days",
+          type: 'text',
           id: uuid(),
           value: function () {
             return this.rawValue;
           },
           rawValue: "",
-          placeholder: "",
-          type: 'text',
-          description: "Maximum periods to search in days",
-          index: 2
+          placeholder: "maxper",
+          index: 2,
         },
         {
           name: "nfreq",
+          description: "Number of trial frequencies to scan",
+          type: 'text',
           id: uuid(),
           value: function () {
             return this.rawValue;
           },
           rawValue: "",
-          placeholder: "",
-          type: 'text',
-          description: "Number of trial frequencies to scan",
-          index: 3
+          placeholder: "nfreq",
+          index: 3,
         },
         {
           name: "nbins",
+          description: "Number of phase bins to break the light curve into (this should be at least 2/qmin)",
+          type: 'text',
           id: uuid(),
           value: function () {
             return this.rawValue;
           },
           rawValue: "",
-          placeholder: "",
-          type: 'text',
-          description: "Number of phase bins to break the light curve into (this should be at least 2/qmin)",
+          placeholder: "nbins",
           index: 4
         },
         {
           name: "timezone",
+          description: "Number (in hours) to add to add to UTC to get the local time",
+          type: 'text',
           id: uuid(),
           value: function () {
             return this.rawValue;
           },
           rawValue: "",
-          placeholder: "",
-          type: 'text',
-          description: "Number (in hours) to add to add to UTC to get the local time",
+          placeholder: "timezone",
           index: 5
         },
         {
           name: "Npeak",
+          description: "number of peaks in the BLS spectrum to find and report",
+          type: 'text',
           id: uuid(),
           value: function () {
             return this.rawValue;
           },
           rawValue: "",
-          placeholder: "",
-          type: 'text',
-          description: "number of peaks in the BLS spectrum to find and report",
+          placeholder: "Npeak",
           index: 6
         },
         {
-          name: "outperiodogram",
+          name: "outperiodogrom",
+          description: "Select to output the BLS period vs. SN spectrum",
+          type: 'checkbox',
           id: uuid(),
           value: function () {
-            return this.rawValue;
+            return boolToInt(this.rawValue);
           },
-          rawValue: "",
-          placeholder: "",
-          type: 'text',
-          description: "set to 1 to output the BLS period vs. SN spectrum",
-          index: 7
+          rawValue: false,
+          index: 7,
         },
         {
           name: "outdir",
+          description: 'output directory for the BLS spectrum if outperiodogram is selected, ".bls" will be appended to the filename',
+          type: 'text',
           id: uuid(),
           value: function () {
             return this.rawValue;
           },
           rawValue: "",
-          placeholder: "",
-          type: 'text',
-          description: 'output directory for the BLS spectrum if outperiodogram is set to 1, ".bls" will be appended to the filename',
-          index: 8
+          placeholder: "outdir",
+          index: 8,
         },
         {
           name: "omodel",
+          description: 'Select to output the model for the light curve, the output directory is then given in modeloutdir (the next argument), the suffix ".bls.model" will be appended to the filename.',
+          type: 'checkbox',
           id: uuid(),
           value: function () {
-            return this.rawValue;
+            return boolToInt(this.rawValue);
           },
-          rawValue: "",
-          placeholder: "",
-          type: 'text',
-          description: 'Set to 1 or zero that can be used to output the model for the light curve, the output directory is then given in modeloutdir, the suffix ".bls.model" will be appended to the filename.',
-          index: 9
+          rawValue: false,
+          index: 9,
         },
         {
           name: "modeloutdir",
+          description: 'Output directory for omodel ".bls.model" files, if omodel is selected',
+          type: 'text',
           id: uuid(),
           value: function () {
             return this.rawValue;
           },
           rawValue: "",
-          placeholder: "",
-          type: 'text',
-          description: 'Output directory for omodel ".bls.model" files',
+          placeholder: "modeloutdir",
           index: 10
         },
         {
           name: "correctlc",
+          description: "Will subtract the transit model from the light curve before passing it to the next command",
+          type: 'checkbox',
           id: uuid(),
           value: function () {
-            if (this.selected) {
-              return "1";
-            } else {
-              return "0";
-            }
+            return boolToInt(this.rawValue);
           },
-          rawValue: "",
-          placeholder: "",
-          selected: false,
-          type: 'checkbox',
-          description: 'Will subtract the transit model from the light curve before passing it to the next command',
-          index: 11
+          rawValue: false,
+          index: 11,
         },
         {
           name: "fittrap",
+          description: "fit a trapezoidal transit to each BLS pea",
+          type: 'checkbox',
           id: uuid(),
           value: function () {
-            if (this.selected) { return "fittrap" } else { return "" }
+            if (this.rawValue) {
+              return "fittrap";
+            } else {
+              return "";
+            }
           },
-          rawValue: "",
-          selected: false,
-          placeholder: "",
-          type: 'checkbox',
-          description: "fit a trapezoidal transit to each BLS pea",
+          rawValue: false,
           index: 12,
         },
         {
           name: "nobinnedrms",
+          description: "Procedure runs faster, but the SN will tend to be suppressed for high significance detections",
+          type: 'checkbox',
           id: uuid(),
           value: function () {
-            if (this.selected) { return "nobinnedrms" } else { return "" }
+            if (this.rawValue) {
+              return "nobinnedrms";
+            } else {
+              return "";
+            }
           },
-          rawValue: "",
-          placeholder: "",
-          selected: false,
-          type: 'checkbox',
-          description: "Procedure runs faster, but the SN will tend to be suppressed for high significance detections",
+          rawValue: false,
           index: 13,
         },
         {
           name: "ophcurve",
+          description: "model phase curve will be output to a file in the directory outdir with suffix '.bls.phcurve'. It will be generated with phases between phmin and phmax and a uniform step size of phstep.",
+          type: 'text',
           id: uuid(),
           value: function () {
             return this.rawValue;
           },
           rawValue: "",
           placeholder: "outdir phmin phmax phstep",
-          type: 'text',
-          description: "model phase curve will be output to a file in the directory outdir with suffix '.bls.phcurve'. It will be generated with phases between phmin and phmax and a uniform step size of phstep.",
           index: 14,
         },
         {
           name: "ojdcurve",
+          description: "model light curve will be output to a file in the directory outdir with suffix '.bls.jdcurve'. The times will be between the first and last times in the light curve with a uniform step size of jdstep.",
+          type: 'text',
           id: uuid(),
           value: function () {
             return this.rawValue;
           },
           rawValue: "",
           placeholder: "outdir jdstep",
-          type: 'text',
-          description: "model light curve will be output to a file in the directory outdir with suffix '.bls.jdcurve'. The times will be between the first and last times in the light curve with a uniform step size of jdstep.",
           index: 15,
         },
         {
           name: "uniform steps",
+          description: "By default the BLS spectrum is sampled at uniform frequency steps. To sample it at uniform steps in period or log(period) use the 'stepP' or 'steplogP' keyword.",
+          type: 'select',
           id: uuid(),
           value: function () {
             if (this.rawValue == "Uniform Frequency Steps (Default)") {
@@ -320,55 +326,50 @@ vartools = {
               return this.rawValue
             }
           },
-          type: "select",
-          options: ["Uniform Frequency Steps (Default)", "stepP", "steplogP"],
           rawValue: "Uniform Frequency Steps (Default)",
-          placeholder: "",
-          description: "By default the BLS spectrum is sampled at uniform frequency steps. To sample it at uniform steps in period or log(period) use the 'stepP' or 'steplogP' keyword.",
+          options: ["Uniform Frequency Steps (Default)", "stepP", "steplogP"],
           index: 16,
         },
         {
           name: "adjust-qmin-by-mindt",
+          description: "Adaptively set the minimum q value to the maximum of qmin or mindt*frequency where mindt is the minimum time difference between consecutive points in the light curve",
+          type: 'text',
           id: uuid(),
           value: function () {
             return this.rawValue;
           },
           rawValue: "",
           placeholder: "adjust-qmin-by-mindt",
-          type: "text",
-          description: "Adaptively set the minimum q value to the maximum of qmin or mindt*frequency where mindt is the minimum time difference between consecutive points in the light curve",
           index: 17,
         },
         {
           name: "reduce-nbins",
+          description: "Adaptively reduce the number of phase bins at each frequency such that there are no more than 2 bins to sample a transit of duration qmin",
+          type: 'checkbox',
           id: uuid(),
           value: function () {
-            if (this.selected) {
+            if (this.rawValue) {
               return "reduce-nbins";
             } else {
               return "";
             }
           },
-          rawValue: "",
-          type: 'checkbox',
-          selected: false,
-          description: "Adaptively reduce the number of phase bins at each frequency such that there are no more than 2 bins to sample a transit of duration qmin",
+          rawValue: false,
           index: 18,
         },
         {
           name: "reportharmonics",
+          description: "Report harmonic frequencies",
+          type: 'checkbox',
           id: uuid(),
           value: function () {
-            if (this.selected) {
+            if (this.rawValue) {
               return "reportharmonics";
             } else {
               return "";
             }
           },
-          rawValue: "",
-          selected: false,
-          type: 'checkbox',
-          description: "Report harmonic frequencies",
+          rawValue: false,
           index: 19,
         },
       ]
